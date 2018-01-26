eb4288b1aa1f4e359ad0a12870eb92ad715c82af
Xiyu <xiyuw@uci.edu>
2018-01-25 21:57:11 -0800

    Initial commit

---
+++ Assets/managescenes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class managescenes : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnStartGame(string sceneName)
+    {
+        Application.LoadLevel("Start");
+    }
+    public void OnStartGame(int sceneIndex)
+    {
+        Application.LoadLevel(sceneIndex);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,15 @@
+Name: Xiyu Wang
+Email: xiyuw@uci.edu
+Student ID: 19001614
+
+
+
+GLASS WORLD
+
+Go ahead to find the wooden coin. Survive and collect as many coins as you can.
+
+Use mouse to adjust your viewing angle. 
+Press "W" "A" "S" "D" OR "¡ü" "¡û" "¡ý" "¡ú" to move around.
+Press SPACE to jump.
+
+I want players to feel like they are in a fantacy world and experience stress, excitement and satisfaction.
